b2c861592ed12f3af5d6df147309a2d037a8e88c
Joao Antonio <joaopsilva.dev@gmail.com>
2022-11-09 15:34:46 -0300

    feat: :sparkles: requisito 7 completo
    função de buscar pela data completa

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 6
@@ -16,7 +17,21 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    format_date = date[8:] + "/" + date[5:7] + "/" + date[:4]
+    try:
+        format = "%Y-%m-%d"
+        datetime.datetime.strptime(date, format)
+        news = search_news({"timestamp": format_date})
+        result = []
+        if news == []:
+            return []
+        else:
+            for new in news:
+                new_tuple = (new["title"], new["url"])
+                result.append(new_tuple)
+            return result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
