960e4ef7fcf3285e02813026a97ba3dc1bfff92b
Joao Antonio <joaopsilva.dev@gmail.com>
2022-11-09 17:02:31 -0300

    feat: :sparkles: requisito 10 completo
    função top_5_news completa

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,9 +1,19 @@
-# from tech_news.database import search_news
+from tech_news.database import get_collection
 
 
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    collection = get_collection()
+    news = list(collection.find({}, {"_id": False}).sort(
+        "comments_count", -1).limit(5))
+    result = []
+    if news == []:
+        return []
+    else:
+        for new in news:
+            new_tuple = (new["title"], new["url"])
+            result.append(new_tuple)
+        return result
 
 
 # Requisito 11
